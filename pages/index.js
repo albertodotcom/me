f85b72e53f694999708b6bcd1df4bbf49d2af459
Alberto Forni <me@albertoforni.com>
2021-01-10 11:09:39 +0100

    Revert "try root folder"
    This reverts commit 4b0147e3e84c6d32ccf911376e932780c652b0c7.

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import App from "../index/App";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Alberto Forni</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:400,600|Roboto:100"
+          rel="stylesheet"
+          key="google-fonts"
+        />
+      </Head>
+      <App />
+    </>
+  );
+}
---
+++ pages/nomo.tsx
@@ -0,0 +1,100 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Alberto Forni</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600"
+          rel="stylesheet"
+          key="google-fonts"
+        />
+      </Head>
+      <div className="page">
+        <h1 className="title">nomo</h1>
+        <h2 className="subtitle">not my own</h2>
+        <p className="description">
+          nomo helps you to track what you lent, borrowed and returned. <br />
+          <br />
+          Use it for books, clothes, baby’s stuff, games or anything you borrow
+          and lend with friends, family or co-workers.
+          <br />
+          <br />
+          Take a picture, add a title, assign someone from your contacts and
+          voilà.
+          <br />
+          <br />
+          Save money and the planet by reducing waste, borrow before you buy and
+          lend to give things new life.
+        </p>
+        <img
+          className="image"
+          src={require("../public/images/dashboard.png")}
+          alt="Picture of the author"
+        />
+      </div>
+      <div>
+        <h3 id="#support">Support</h3>
+        <p>
+          if you need help or you would like a new feature, just send an email
+          at <a href="mailto:nomo@ahaloop.com">nomo@ahaloop.com</a> and we will
+          do our best to help!
+        </p>
+      </div>
+      <div>
+        <h3 id="#privacy">Privacy</h3>
+        <p>
+          We don't collect any kind of information. Everything runs on the
+          user's device and stays there!
+        </p>
+      </div>
+
+      <style jsx>{`
+        .page {
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          grid-template-areas:
+            "title title"
+            "subtitle subtitle"
+            "description image";
+        }
+        .title {
+          grid-area: title;
+          text-align: center;
+          margin: 0;
+        }
+        .subtitle {
+          grid-area: subtitle;
+          text-align: center;
+          margin: 0;
+        }
+        .description {
+          grid-area: description;
+        }
+        .image {
+          grid-area: image;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          background-color: #04226f;
+          color: #ffffff;
+          font-family: "Open Sans";
+          max-width: 1024px;
+          margin: 0 auto;
+        }
+        a,
+        a:hover,
+        a:visited {
+          color: #ffffff;
+        }
+        p {
+          font-weight: 300;
+        }
+      `}</style>
+    </>
+  );
+}
